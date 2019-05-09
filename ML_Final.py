9bdb8586a6d51c2feb0e512a88bca2b6a7aefa0e
Nicole Laczny <nlaczn2@uic.edu>
2019-05-09 17:34:52 -0500

    in ncase i blue screen

--- ML_Final.py
+++ ML_Final.py
@@ -1,1 +1,67 @@
 #412 be weird
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as mp
+from pylab import show
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection._validation import cross_val_score
+from sklearn import preprocessing # import normalize
+from sklearn.preprocessing import normalize
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn import datasets
+# Import train_test_split function
+from sklearn.model_selection import train_test_split
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+#data = np.loadtxt("iris.data")
+#from sklearn.datasets import load_iris
+
+#iris = load_iris()
+#type(iris) 
+#print (iris.data)
+#iris.data.shape
+
+#print (iris.feature_names)
+#print (iris.target)
+#print (iris.target_names)
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+print(iris.data)
+print(iris.target)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3) # 70% training and 30% test
+
+# Create adaboost classifer object
+abc = AdaBoostClassifier(n_estimators=50,
+                         learning_rate=1)
+# Train Adaboost Classifer
+model = abc.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = model.predict(X_test)
+
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+from sklearn.ensemble import AdaBoostClassifier
+
+# Import Support Vector Classifier
+from sklearn.svm import SVC
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+svc=SVC(probability=True, kernel='linear')
+
+# Create adaboost classifer object
+abc =AdaBoostClassifier(n_estimators=50, base_estimator=svc,learning_rate=1)
+
+# Train Adaboost Classifer
+model = abc.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = model.predict(X_test)
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
