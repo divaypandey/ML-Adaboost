e7b36aec5047196ad9c1ab52ad5a6a6952a86200
Nicole Laczny <nlaczn2@uic.edu>
2019-05-10 09:57:16 -0500

    preped code for lowest error testing in ADA

--- ML_Final.py
+++ ML_Final.py
@@ -9,6 +9,8 @@ from sklearn.model_selection._validation import cross_val_score
 from sklearn import preprocessing # import normalize
 from sklearn.preprocessing import normalize
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn import datasets
 # Import train_test_split function
@@ -35,17 +37,83 @@ print(iris.data)
 print(iris.target)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3) # 70% training and 30% test
 
-# Create adaboost classifer object
-abc = AdaBoostClassifier(n_estimators=50,
-                         learning_rate=1)
-# Train Adaboost Classifer
-model = abc.fit(X_train, y_train)
+print()
+print("testing for depth 1")
 
-#Predict the response for test dataset
-y_pred = model.predict(X_test)
+## Model Accuracy, how often is the classifier correct?
+
+#lNodes = [1,5,10,100,1000,10000]
+#AccAvgs = []
+#errorAAvgs = []
+#for node in lNodes:
+#    estimator = DecisionTreeClassifier(max_depth = 1)
+#    # Create adaboost classifer object
+#    abc = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
+#    # Train Adaboost Classifer
+#    modelA = abc.fit(X_train, y_train)
+#    #Predict the response for test dataset
+#    y_pred = modelA.predict(X_test)
+#    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+#    #adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
+#    #modelA = adaboost.fit(normVal,trainDigits)
+#    errorA = cross_val_score(modelA,X_train,y_train,cv = 10)
+#    errorsinA = [1-x for x in errorA]
+#    AvgError = np.mean(errorsinA)
+#    errorAAvgs.append(AvgError)
+#    AccAvg = np.mean(errorA)
+#    AccAvgs.append(AccAvg)
+#    print("Adaboost depth 1 for %d" %node ,errorA)
+    
+
+
+#mp.plot(lNodes,AccAvgs,'o')
+#mp.title("Figure 1 - Accuracy")
+#mp.xlabel("# Nodes")
+#mp.ylabel("Accuracy")
+#mp.xscale('log')
+#show()
+
+
+print()
+print("testing for depth 1")
 
 # Model Accuracy, how often is the classifier correct?
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+lNodes = [1,5,10,100,1000,10000]
+AccAvgs = []
+errorAAvgs = []
+for node in lNodes:
+    estimator = DecisionTreeClassifier(max_depth = 2)
+    # Create adaboost classifer object
+    abc = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
+    # Train Adaboost Classifer
+    modelA = abc.fit(X_train, y_train)
+    #Predict the response for test dataset
+    y_pred = modelA.predict(X_test)
+    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+    #adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
+    #modelA = adaboost.fit(normVal,trainDigits)
+    errorA = cross_val_score(modelA,X_train,y_train,cv = 10)
+    errorsinA = [1-x for x in errorA]
+    AvgError = np.mean(errorsinA)
+    errorAAvgs.append(AvgError)
+    AccAvg = np.mean(errorA)
+    AccAvgs.append(AccAvg)
+    print("Adaboost depth 1 for %d" %node ,errorA)
+    
+
+
+mp.plot(lNodes,AccAvgs,'o')
+mp.title("Figure 1 - Accuracy")
+mp.xlabel("# Nodes")
+mp.ylabel("Accuracy")
+mp.xscale('log')
+show()
+
+
+
 
 from sklearn.ensemble import AdaBoostClassifier
 
@@ -65,3 +133,62 @@ model = abc.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+
+######################################## test for lowest error ###############
+estimator = DecisionTreeClassifier(max_depth = 1)
+adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=1000)
+modelA = adaboost.fit(normVal,trainDigits)
+errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
+errorsinA = [1-x for x in errorA]
+AvgError = np.mean(errorsinA)
+print("Adaboost depth 1, est: 1000 error:" ,AvgError)
+
+estimator = DecisionTreeClassifier(max_depth = 10)
+adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=100)
+modelA = adaboost.fit(normVal,trainDigits)
+errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
+errorsinA = [1-x for x in errorA]
+AvgError = np.mean(errorsinA)
+print("Adaboost depth 10, est: 100 error:" ,AvgError)
+
+
+estimator = DecisionTreeClassifier(max_depth = 1000)
+adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=100)
+modelA = adaboost.fit(normVal,trainDigits)
+errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
+errorsinA = [1-x for x in errorA]
+AvgError = np.mean(errorsinA)
+print("Adaboost depth 1000, est: 100 error:" ,AvgError)
+
+
+
+############################# predict on lowest error ###########################
+#estimator = DecisionTreeClassifier(max_depth = 1)
+#adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=1000)
+#adaboost.fit(normVal,trainDigits)
+
+#xPred = []
+#yPred = []
+#cPred = []
+
+###################################### official graph but takes forever to run #####3
+#for xP in range(-100,100):
+#    xP = xP/100.0
+#    for yP in range(-100,100):
+#        yP = yP/100.0
+#        xPred.append(xP)
+#        yPred.append(yP)
+#        print('currently predicting', xP, yP)
+#        if(adaboost.predict([[xP,yP]])=="1.0"):
+#            cPred.append("r")
+#        else:
+#            cPred.append("b")
+#mp.scatter(X1,Y1,s=3, c=colors)
+#mp.scatter(xPred,yPred,s=3,c=cPred,alpha=.2)
+
+#################   graph  #################
+#mp.title("Figure 4.10 (for depth:1 # estimators: 1000)")
+#mp.xlabel("Average")
+#mp.ylabel("Varience")
+#show()
