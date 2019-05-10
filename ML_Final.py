e45ba754f0f18aa7ecfdd41b6a2a245ee74d03da
Nicole Laczny <nlaczn2@uic.edu>
2019-05-10 12:03:46 -0500

    Signed-off-by: Nicole Laczny <nlaczn2@uic.edu>

--- ML_Final.py
+++ ML_Final.py
@@ -17,6 +17,11 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 #Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
+
+from scipy.io import arff
+import pandas as pd
+
+
 #data = np.loadtxt("iris.data")
 #from sklearn.datasets import load_iris
 
@@ -75,91 +80,91 @@ print("testing for depth 1")
 #show()
 
 
-print()
-print("testing for depth 1")
+#print()
+#print("testing for depth 1")
 
-# Model Accuracy, how often is the classifier correct?
+## Model Accuracy, how often is the classifier correct?
 
-lNodes = [1,5,10,100,1000,10000]
-AccAvgs = []
-errorAAvgs = []
-for node in lNodes:
-    estimator = DecisionTreeClassifier(max_depth = 2)
-    # Create adaboost classifer object
-    abc = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
-    # Train Adaboost Classifer
-    modelA = abc.fit(X_train, y_train)
-    #Predict the response for test dataset
-    y_pred = modelA.predict(X_test)
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+#lNodes = [1,5,10,100,1000,10000]
+#AccAvgs = []
+#errorAAvgs = []
+#for node in lNodes:
+#    estimator = DecisionTreeClassifier(max_depth = 2)
+#    # Create adaboost classifer object
+#    abc = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
+#    # Train Adaboost Classifer
+#    modelA = abc.fit(X_train, y_train)
+#    #Predict the response for test dataset
+#    y_pred = modelA.predict(X_test)
+#    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-    #adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
-    #modelA = adaboost.fit(normVal,trainDigits)
-    errorA = cross_val_score(modelA,X_train,y_train,cv = 10)
-    errorsinA = [1-x for x in errorA]
-    AvgError = np.mean(errorsinA)
-    errorAAvgs.append(AvgError)
-    AccAvg = np.mean(errorA)
-    AccAvgs.append(AccAvg)
-    print("Adaboost depth 1 for %d" %node ,errorA)
+#    #adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=node)
+#    #modelA = adaboost.fit(normVal,trainDigits)
+#    errorA = cross_val_score(modelA,X_train,y_train,cv = 10)
+#    errorsinA = [1-x for x in errorA]
+#    AvgError = np.mean(errorsinA)
+#    errorAAvgs.append(AvgError)
+#    AccAvg = np.mean(errorA)
+#    AccAvgs.append(AccAvg)
+#    print("Adaboost depth 1 for %d" %node ,errorA)
     
 
 
-mp.plot(lNodes,AccAvgs,'o')
-mp.title("Figure 1 - Accuracy")
-mp.xlabel("# Nodes")
-mp.ylabel("Accuracy")
-mp.xscale('log')
-show()
+#mp.plot(lNodes,AccAvgs,'o')
+#mp.title("Figure 1 - Accuracy")
+#mp.xlabel("# Nodes")
+#mp.ylabel("Accuracy")
+#mp.xscale('log')
+#show()
 
 
 
 
-from sklearn.ensemble import AdaBoostClassifier
+#from sklearn.ensemble import AdaBoostClassifier
 
-# Import Support Vector Classifier
-from sklearn.svm import SVC
-#Import scikit-learn metrics module for accuracy calculation
-from sklearn import metrics
-svc=SVC(probability=True, kernel='linear')
+## Import Support Vector Classifier
+#from sklearn.svm import SVC
+##Import scikit-learn metrics module for accuracy calculation
+#from sklearn import metrics
+#svc=SVC(probability=True, kernel='linear')
 
-# Create adaboost classifer object
-abc =AdaBoostClassifier(n_estimators=50, base_estimator=svc,learning_rate=1)
+## Create adaboost classifer object
+#abc =AdaBoostClassifier(n_estimators=50, base_estimator=svc,learning_rate=1)
 
-# Train Adaboost Classifer
-model = abc.fit(X_train, y_train)
+## Train Adaboost Classifer
+#model = abc.fit(X_train, y_train)
 
-#Predict the response for test dataset
-y_pred = model.predict(X_test)
+##Predict the response for test dataset
+#y_pred = model.predict(X_test)
 
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-
-
-######################################## test for lowest error ###############
-estimator = DecisionTreeClassifier(max_depth = 1)
-adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=1000)
-modelA = adaboost.fit(normVal,trainDigits)
-errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
-errorsinA = [1-x for x in errorA]
-AvgError = np.mean(errorsinA)
-print("Adaboost depth 1, est: 1000 error:" ,AvgError)
-
-estimator = DecisionTreeClassifier(max_depth = 10)
-adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=100)
-modelA = adaboost.fit(normVal,trainDigits)
-errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
-errorsinA = [1-x for x in errorA]
-AvgError = np.mean(errorsinA)
-print("Adaboost depth 10, est: 100 error:" ,AvgError)
+#print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
 
-estimator = DecisionTreeClassifier(max_depth = 1000)
-adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=100)
-modelA = adaboost.fit(normVal,trainDigits)
-errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
-errorsinA = [1-x for x in errorA]
-AvgError = np.mean(errorsinA)
-print("Adaboost depth 1000, est: 100 error:" ,AvgError)
+######################################### test for lowest error ###############
+#estimator = DecisionTreeClassifier(max_depth = 1)
+#adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=1000)
+#modelA = adaboost.fit(normVal,trainDigits)
+#errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
+#errorsinA = [1-x for x in errorA]
+#AvgError = np.mean(errorsinA)
+#print("Adaboost depth 1, est: 1000 error:" ,AvgError)
+
+#estimator = DecisionTreeClassifier(max_depth = 10)
+#adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=100)
+#modelA = adaboost.fit(normVal,trainDigits)
+#errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
+#errorsinA = [1-x for x in errorA]
+#AvgError = np.mean(errorsinA)
+#print("Adaboost depth 10, est: 100 error:" ,AvgError)
+
+
+#estimator = DecisionTreeClassifier(max_depth = 1000)
+#adaboost = AdaBoostClassifier(base_estimator=estimator,n_estimators=100)
+#modelA = adaboost.fit(normVal,trainDigits)
+#errorA = cross_val_score(modelA,normVal,trainDigits,cv = 10)
+#errorsinA = [1-x for x in errorA]
+#AvgError = np.mean(errorsinA)
+#print("Adaboost depth 1000, est: 100 error:" ,AvgError)
 
 
 
@@ -192,3 +197,13 @@ print("Adaboost depth 1000, est: 100 error:" ,AvgError)
 #mp.xlabel("Average")
 #mp.ylabel("Varience")
 #show()
+
+
+data = arff.loadarff('3year.arff')
+df = pd.DataFrame(data[0])
+df2= pd.DataFrame(data[0])
+
+df.head()
+df.drop("class", axis=1, inplace=True)
+print(df)
+print(df.dtypes)
